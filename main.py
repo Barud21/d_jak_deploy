85d1f954098fb9151d9fb554fab485681cd335b2
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-04-05 13:32:32 +0200

    Zadanie 3.

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from dataclasses import dataclass
-from typing import Dict
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+app.counter = 0
+app.patients = []
+
+##############################################################
+# Zadanie 1
+##############################################################
+
 @app.get('/')
 def hello_world():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
 
 ##############################################################
 # Zadanie 2
 ##############################################################
 
-# @app.get('/method')
-# def return_method_name():
-#
-#     return {"method": "METHOD"}
+@app.get('/method')
+def return_get():
+    return {"method": "GET"}
+
+@app.post('/method')
+def return_post():
+    return {"method": "POST"}
+
+@app.put('/method')
+def return_put():
+    return {"method": "PUT"}
+
+@app.delete('/method')
+def return_delete():
+    return {"method": "DELETE"}
+
 
 ##############################################################
 # Zadanie 3
@@ -28,15 +46,16 @@ class patient_data_rq(BaseModel):
 
 
 class patient_data_resp(BaseModel):
-    id: int = 0
-    patient: Dict
+    id: int
+    patient: patient_data_rq
 
-#TODO: zrobić licznik przechowujący liczbę wejść na stronę
+# #TODO: zrobić licznik przechowujący liczbę wejść na stronę
 
 @app.post('/patient')
 def patient_data(rq: patient_data_rq):
-    #rp.id = rp.id + 1 #TODO: czemu to nie działa
-    return patient_data_resp(patient=rq.dict())
+    app.patients.append(rq)
+    app.counter += 1
+    return app.patients
 
 ##############################################################
 # Zadanie 4
@@ -45,27 +64,30 @@ def patient_data(rq: patient_data_rq):
 
 
 
+
+
+
+
 ##############################################################
 ##############################################################
 
-
-class HelloNameResp(BaseModel):
-    message: str
-
-@app.get('/hello/{name}', response_model=HelloNameResp)
-def hello_name(name: str):
-    return HelloNameResp(message=f"Hello {name}")
-
-
-class GiveMeSomethingRq(BaseModel):
-    first_key: str
-
-
-class GiveMeSomethingResp(BaseModel):
-    received: Dict
-    constant_data: str = "python jest super"
-
-
-@app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
-def receive_something(rq: GiveMeSomethingRq):
-    return GiveMeSomethingResp(received=rq.dict())
+# class HelloNameResp(BaseModel):
+#     message: str
+#
+# @app.get('/hello/{name}', response_model=HelloNameResp)
+# def hello_name(name: str):
+#     return HelloNameResp(message=f"Hello {name}")
+#
+#
+# class GiveMeSomethingRq(BaseModel):
+#     first_key: str
+#
+#
+# class GiveMeSomethingResp(BaseModel):
+#     received: Dict
+#     constant_data: str = "python jest super"
+#
+#
+# @app.post("/dej/mi/coś", response_model=GiveMeSomethingResp)
+# def receive_something(rq: GiveMeSomethingRq):
+#     return GiveMeSomethingResp(received=rq.dict())
--- test_main.py
+++ test_main.py
@@ -5,27 +5,57 @@ import pytest
 
 client = TestClient(app)
 
+##############################################################
+# Zadanie 1
+##############################################################
+
 def test_hello_world():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World during the coronavirus pandemic!"}
 
 
+##############################################################
+# Zadanie 2
+##############################################################
+
+def test_return_get():
+    response = client.get('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "GET"}
+
+def test_return_post():
+    response = client.post('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "POST"}
+
+def test_return_put():
+    response = client.put('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "PUT"}
+
+def test_return_delete():
+    response = client.delete('/method')
+    assert response.status_code == 200
+    assert response.json() == {"method": "DELETE"}
+
 
 ##############################################################
 # Zadanie 3
 ##############################################################
 
-#@pytest.mark.parametrize("id", [0])
-def test_patient_data():
-    response = client.post('/patient', json={"name": "Bartek", "surename": "Rudzinski"})
-    assert response.status_code == 200
-    assert response.json() == {"id": 0, "patient": {"name": "Bartek", "surename": "Rudzinski"}}
+# def test_patient_data(counter):
+#     response = client.post('/patient', json={"name": "Bartek", "surename": "Rudzinski"})
+#     assert response.status_code == 200
+#     assert response.json() == {"id": 0, "patient": {"name": "Bartek", "surename": "Rudzinski"}}
 
 
 ##############################################################
 # Zadanie 4
 ##############################################################
+
+
+
 
 
 @pytest.mark.parametrize("name", ["Ala", "Zażółć Gęślą jaźń", "Grzegorz Brzęczyszczykiewicz"])
